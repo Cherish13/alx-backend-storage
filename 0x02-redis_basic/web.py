6ba3826a32fe55958232913e73726a601866f641
Shinene <cherishlyte13@gmail.com>
2023-07-19 20:42:47 +0100

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-import redis
 import requests
+import redis
+import time
 from functools import wraps
-from typing import Callable
 
+# Initialize Redis client
+redis_client = redis.Redis()
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+def cache_result(expires=10):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(url):
+            # Check if the URL is already cached
+            cached_result = redis_client.get(f"cache:{url}")
+            if cached_result:
+                return cached_result.decode()
 
+            # Invoke the original function if the URL is not cached
+            result = func(url)
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+            # Cache the result with the specified expiration time
+            redis_client.set(f"cache:{url}", result, ex=expires)
 
+            return result
+        return wrapper
+    return decorator
 
-@data_cacher
+@cache_result()
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    # Simulate slow response using slowwly.robertomurray.co.uk
+    response = requests.get(url)
+    return response.text
